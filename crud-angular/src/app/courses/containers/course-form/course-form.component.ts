9846c289a9433cf2dcf6d239fe3a0b8ebf39a54a
gustas01 <gustavohdlima@hotmail.com>
2023-07-25 16:05:18 -0300

    Feat: Adicionando validações no FormArray das aulas

--- crud-angular/src/app/courses/containers/course-form/course-form.component.ts
+++ crud-angular/src/app/courses/containers/course-form/course-form.component.ts
@@ -26,7 +26,7 @@ export class CourseFormComponent implements OnInit{
     _id: [this.course._id],
     name: [this.course.name, [Validators.required, Validators.minLength(5), Validators.maxLength(100)]],
     category: [this.course.category, [Validators.required]],
-    lessons: this.formBuilder.array(this.retrieveLessons(this.course))
+    lessons: this.formBuilder.array(this.retrieveLessons(this.course), Validators.required)
     });
   }
 
@@ -43,8 +43,8 @@ export class CourseFormComponent implements OnInit{
   private createLesson(lesson: Lesson = {id: '', name: '', youtubeUrl: ''}){
     return this.formBuilder.group({
       id: [lesson.id],
-      name: [lesson.name],
-      youtubeUrl: [lesson.youtubeUrl]
+      name: [lesson.name, [Validators.required, Validators.minLength(5), Validators.maxLength(100)]],
+      youtubeUrl: [lesson.youtubeUrl, [Validators.required, Validators.minLength(10), Validators.maxLength(11)]]
     });
   }
 
@@ -63,10 +63,14 @@ export class CourseFormComponent implements OnInit{
   }
 
   onSubmit(){
-    this.courseService.save(this.form.value).subscribe({
-      next: result => this.onSuccess(),
-      error: error => this.onError()
-    })
+    if(this.form.valid)
+      this.courseService.save(this.form.value).subscribe({
+        next: result => this.onSuccess(),
+        error: error => this.onError()
+      });
+    else {
+      alert('Formulário inválido!')
+    }
   }
 
   onCancel(){
@@ -99,5 +103,10 @@ export class CourseFormComponent implements OnInit{
     }
 
     return 'Campo inválido!'
+  }
+
+  isFormArrayRequired(){
+    const lessons = this.form.get('lessons') as UntypedFormArray;
+    return !lessons.valid && lessons.hasError('required') && lessons.touched;
   }
 }
